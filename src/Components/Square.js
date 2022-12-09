12ccd94fb312aaf7c5c4f5acbbd143104d498c64
pradipdhale <pradipdhale127@gmail.com>
2022-12-09 17:22:46 +0530

    done all

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import './square.css'
+import React from "react";
+import "./square.css";
 
-const Square = ({ value }) => {
-  return <button type='button'className='square'>{ value }</button>
-}
+const Square = ({ value, onClick }) => {
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+    
+  );
+};
 
-export default Square
+export default Square;
